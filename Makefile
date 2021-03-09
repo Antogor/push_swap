4d5059bd7593806dc4c17abe663e9f7263209a40
Antogor <lagarto1990@hotmail.com>
2021-03-09 20:05:27 +0100

    gets arguments

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
 NAME = checker
 
-INCLUDES = ./MyLIBFT/libft/libft.h
+INCLUDES = MyLIBFT/libft/libft.h
 
-FLAGS = gcc -I
+FLAGS = gcc
 
 FUN = test.c
 
 OBJ = $(FUN:.c=.o)
 
-LIBFT = ./MyLIBFT/libft.a
-MYLIBFT = ./MyLIBFT/
+LIBFT = MyLIBFT/libft.a
+MYLIBFT = MyLIBFT/
 
 all: $(NAME)
 
 $(LIBFT): 
-	@$(MAKE) -C ./MyLIBFT/
+	@$(MAKE) -C $(MYLIBFT)
 
 $(NAME): $(OBJ) $(LIBFT) $(INCLUDES)
-	@$(FLAGS) $(FUN) $(LIBFT) -L$(MYLIBFT) #-o $(NAME)
+	@$(FLAGS) $(FUN) $(LIBFT) -o $(NAME)
--- test.c
+++ test.c
@@ -1,7 +1,28 @@
-#include "./MyLIBFT/libft/libft.h"
+#include "MyLIBFT/libft/libft.h"
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	ft_printf("hola");
+	int cont;
+	int cont2;
+	int number;
+	int pila_a[argc];
+	int pila_b[argc];
+
+	if(argc == 1)
+		return 1;
+	cont = 1;
+	cont = 0;
+	while(argv[cont])	
+	{
+		number = ft_atoi(argv[cont]);
+		ft_printf("%d", number);
+		if(ft_isdigit(number) == 0)
+		{
+			ft_printf("Error\n");
+			return 1;
+		}
+		pila_a[cont2++] = number;
+		cont++;
+	}
 	return 0;
 }
